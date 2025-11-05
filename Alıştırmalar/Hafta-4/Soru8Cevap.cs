85b2988ded33de294a28804ed74f47518b6a1289
Arda8034 <arslanarda540@gmail.com>
2025-11-06 00:46:59 +0300

    Update Soru8Cevap.cs

--- Alıştırmalar/Hafta-4/Soru8Cevap.cs
+++ Alıştırmalar/Hafta-4/Soru8Cevap.cs
@@ -1,1 +1,22 @@
+namespace Bos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int toplam = 0;
+
+            while (sayi > 0)
+            {
+                int rakam = sayi % 10;
+                toplam += rakam;
+                sayi /= 10;
+            }
+            Console.WriteLine("Rakamların toplamı: " + toplam);
+        }
+    }
+}
 
